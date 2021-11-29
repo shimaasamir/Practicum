65d6e3a9f8e3ef932b55894e7cb32eaf161176f2
shimaasamir <shimaasamir88@hotmail.com>
2021-11-29 19:16:42 +0200

    exercise2

--- src/app/exercise2/exercise2.component.ts
+++ src/app/exercise2/exercise2.component.ts
@@ -1,50 +1,59 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
-  selector: 'app-exercise2',
+  selector: "app-exercise2",
   template: `
     <div class="parent">
       <header><h1 contenteditable>Header</h1></header>
-      <div class="left-side" contenteditable>Left Sidebar</div>
-      <main contenteditable>🐱‍👤</main>
-      <div class="right-side" contenteditable>Right Sidebar</div>
+      <div class="content-container">
+        <div class="left-side" contenteditable>Left Sidebar</div>
+        <main contenteditable>🐱‍👤</main>
+        <div class="right-side" contenteditable>Right Sidebar</div>
+      </div>
       <footer contenteditable>Footer Content</footer>
     </div>
   `,
-  styles: [`
-    .parent {
-      height: 100%;
-      width: 100%;
-    }
-    
-    header {
-      background: lightpink;
-    }
+  styles: [
+    `
+      .parent {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+      }
 
-    .left-side {
-      background: lightblue;
-    }
+      header {
+        background: lightpink;
+      }
+      .content-container {
+        display: flex;
+        flex-grow: 1;
+      }
+      .left-side {
+        background: lightblue;
+      }
 
-    main {
-      background: coral;
-      font-size: 5rem;
-    }
+      main {
+        background: coral;
+        font-size: 5rem;
+        flex-grow: 1;
+      }
 
-    .right-side {
-      background: yellow;
-    }
+      .right-side {
+        background: yellow;
+      }
 
-    footer {
-      background: wheat;
-    }
-  `],
-  changeDetection: ChangeDetectionStrategy.OnPush
+      footer {
+        background: wheat;
+        text-align: center;
+        padding: 5px;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Exercise2Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
