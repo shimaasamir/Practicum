4c5ec8253ea66d8007c16b44495d1ac051acb3a9
shimaasamir <shimaasamir88@hotmail.com>
2021-11-29 19:12:56 +0200

    exercise1

--- src/app/exercise1/exercise1.component.ts
+++ src/app/exercise1/exercise1.component.ts
@@ -1,44 +1,42 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
-  selector: 'app-exercise1',
+  selector: "app-exercise1",
   template: `
-  <div class="parent blue">
-    <div class="child box coral" contenteditable>
-      😋
+    <div class="parent blue">
+      <div class="child box coral" contenteditable>😋</div>
     </div>
-  </div>
   `,
-  styles: [`
-  .parent {
-    display: grid;
-    background: lightblue;
-    width: 100%;
-    height: 100%;
-    min-width: 500px;
-    min-height: 500px;
-    overflow: auto;
-  }
+  styles: [
+    `
+      .parent {
+        display: grid;
+        background: lightblue;
+        width: 100%;
+        height: 100%;
+        min-width: fit-content;
+        min-height: fit-content;
+        overflow: auto;
+        justify-items: center;
+      }
 
-  .child {
-    padding: 0.5rem;
-    border-radius: 10px;
-    border: 1px solid red;
-    background: lightpink;
-    font-size: 2rem;
-    text-align: center;
-    width: fit-content;
-    height: fit-content;
-  }`
-  
+      .child {
+        padding: 0.5rem;
+        border-radius: 10px;
+        border: 1px solid red;
+        background: lightpink;
+        font-size: 2rem;
+        text-align: center;
+        width: fit-content;
+        height: fit-content;
+        place-self: center;
+      }
+    `,
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Exercise1Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
